5c13c6b564a32a3c081755b53d5ced446acdb7b9
Ahmed Maher <engahmedmaheer@gmail.com>
2023-08-29 20:55:32 +0300

    NUCLEO SCB DRIVE

---
+++ Nucleo/MCAL/SCB/SRC/SCB_Program.c
@@ -0,0 +1,35 @@
+/*
+ * SCB_Program.c
+ *
+ *  Created on: Jul 7, 2023
+ *      Author: AhmedMaher
+ */
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ErrType.h"
+#include "STM32F446xx.h"
+#include "SCB_Private.h"
+#include "SCB_Interface.h"
+
+
+
+/**
+ * @brief : Function To Set Group Priority
+ * @pre :
+ * @post :
+ * @param   GroupPriority
+ * @return: LocalErrorStatus
+ */
+uint8_t SCB_U8SetPriorityGroup(PRIORTY_E GroupPriority)
+{
+
+	uint8_t LocalErrorState = OK;
+
+	SCB_AIRCR=GroupPriority;
+
+
+	return LocalErrorState ;
+
+}
+
