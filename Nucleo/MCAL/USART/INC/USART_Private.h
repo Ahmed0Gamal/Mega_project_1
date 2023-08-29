75e7b164693921659160415f8681f1fddb49b616
Ahmed Maher <engahmedmaheer@gmail.com>
2023-08-29 21:02:45 +0300

    NUCLEO USART Drive

---
+++ Nucleo/MCAL/USART/INC/USART_Private.h
@@ -0,0 +1,48 @@
+/*******************************************************************/
+/*******************************************************************/
+/*****************   @Author:  AhmedMaher		********************/
+/*****************   @Layer:   MCAL				********************/
+/*****************   @SWC:     USART			********************/
+/*****************	 @Version: 1.00				********************/
+/*******************************************************************/
+/*******************************************************************/
+
+
+#ifndef USART_PRIVATE_H_
+#define USART_PRIVATE_H_
+
+
+
+
+
+
+#define CR_MASK 			0b11111
+
+#define CR_PIN_ACCESS			4
+
+
+#define ENABLE_MASK				1
+#define DISABLE_MASK			0
+
+
+#define BIT_MASK					0b1
+#define TWO_BIT_MASK				0b11
+#define THREE_BIT_MASK				0b111
+#define FOUR_BIT_MASK				0b1111
+
+
+
+
+#define TXE_BIT					7
+
+#define TC_BIT					6
+
+#define RXNE_BIT				5
+
+
+
+
+
+
+
+#endif /* USART_PRIVATE_H_ */
